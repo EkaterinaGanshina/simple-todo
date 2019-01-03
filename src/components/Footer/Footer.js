5f5094738ea8a18d3d068cea921406890549c7de
Ekaterina Ganshina <ekaterina.ganshina@bk.ru>
2019-01-03 20:47:13 +0300

    Added footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+export const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <a className={styles.footerLink} href="http://ganshina.com">Go to main page</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
